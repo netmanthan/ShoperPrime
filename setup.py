979346921a56ac8526f46bd65b4baf51c3fab36b
Jawahar Ramkripal Mallah <jawahar.mallah@netmanthan.com>
2023-10-03 03:45:02 +0530

    ShoperPrime Point Of Sales
    Shoper 9 Redefined

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
-# TODO: Remove this file when  v15.0.0 is released
-from setuptools import setup
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
 
-name = "erpnext"
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
 
-setup()
+# get version from __version__ variable in shoperprimepos/__init__.py
+from shoperprimepos import __version__ as version
+
+setup(
+    name="shoperprimepos",
+    version=version,
+    description="ShoperPrime POS",
+    author="Jawahar R Mallah",
+    author_email="netmanthan@outlook.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires,
+)
